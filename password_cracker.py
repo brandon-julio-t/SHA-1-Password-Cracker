292f908ab39fff4483cad3358752775d450586ff
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-06-09 04:43:23 +0000

    refactor

--- password_cracker.py
+++ password_cracker.py
@@ -1,14 +1,25 @@
 import hashlib
 
 
-def crack_sha1_hash(hash, use_salts=False):
-    passwords = read_all_lines_in_file('top-10000-passwords.txt')
-    salts = read_all_lines_in_file('known-salts.txt')
+def read_all_lines_in_file(filename):
+    data = []
+    with open(filename) as f:
+        for line in f:
+            data.append(line.strip())
+    return data
 
+
+passwords = read_all_lines_in_file('top-10000-passwords.txt')
+salts = read_all_lines_in_file('known-salts.txt')
+
+
+def crack_sha1_hash(hash, use_salts=False):
     for password in passwords:
-        result = ''
-        
-        if use_salts:
+        if not use_salts:
+            result = hashlib.sha1(password.encode()).hexdigest()
+            if (hash == result):
+                return password
+        else:
             for salt in salts:
                 possibilities = [
                     salt + password,
@@ -20,16 +31,5 @@ def crack_sha1_hash(hash, use_salts=False):
                     result = hashlib.sha1(possibility.encode()).hexdigest()
                     if (hash == result):
                         return password
-        else:
-            result = hashlib.sha1(password.encode()).hexdigest()
-            if (hash == result):
-                return password
-        
-    return 'PASSWORD NOT IN DATABASE'
 
-def read_all_lines_in_file(filename):
-    data = []
-    with open(filename) as f:
-        for line in f:
-            data.append(line.strip())
-    return data
+    return 'PASSWORD NOT IN DATABASE'
